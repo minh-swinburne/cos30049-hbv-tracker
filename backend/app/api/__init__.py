6da4baa3c8f5a2d4cc95c100b40919dcd03573d2
minh-swinburne <140883075+minh-swinburne@users.noreply.github.com>
2025-03-11 11:10:15 +0700

    chore: update requirements.txt

--- backend/app/api/__init__.py
+++ backend/app/api/__init__.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
+from . import auth
 from . import graph
 
 router = APIRouter(prefix="/api")
 
+router.include_router(auth.router)
 router.include_router(graph.router)
